45ae186a56b22dee0d6e3049d1a2b8bdb9ab074e
Andrew121410 <andrewcherring@gmail.com>
2026-03-26 19:23:48 -0400

    Update gradle.yml && fix built in updater.

--- src/main/java/com/andrew121410/mc/world16essentials/Updater.java
+++ src/main/java/com/andrew121410/mc/world16essentials/Updater.java
@@ -4,7 +4,7 @@ import com.andrew121410.ccutils.utils.HashBasedUpdater;
 
 public class Updater extends HashBasedUpdater {
 
-    private static final String JAR_URL = "https://github.com/World1-6/Legacy-World1-6Essentials/releases/download/latest/Legacy-World1-6Essentials+java8.jar";
+    private static final String JAR_URL = "https://github.com/World1-6/Legacy-World1-6Essentials/releases/download/latest/Legacy-World1-6Essentials.jar";
     private static final String HASH_URL = "https://github.com/World1-6/Legacy-World1-6Essentials/releases/download/latest/hash.txt";
 
     public Updater(World16Essentials plugin) {
